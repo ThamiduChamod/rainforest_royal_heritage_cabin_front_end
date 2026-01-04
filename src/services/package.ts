84cd3934bd9fa2eeeaffccd7c607806e29aebbe3
ThamiduChamod <thamiduchamod748@gmail.com>
2026-01-04 16:46:16 +0530

    update package.ts

---
+++ src/services/package.ts
@@ -0,0 +1,18 @@
+import api from "./api"
+
+export const addPackage = async ( data: any) => {
+    
+    const res = await api.post("/Post/createPackage",data, {
+        headers: {
+        "Content-Type": "multipart/form-data"
+    }
+    })
+    return res.data
+}
+
+export const getAllPackage = async () =>{
+    const res = await api.get("/post/getAllPackage")
+    return res.data
+}
+
+
